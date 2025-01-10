1f467b079f3cabc6a705d52ade74770db410bb59
m-mate <m.mate@mailbox.unideb.hu>
2025-01-10 17:57:04 +0200

    add:RouteController

---
+++ src/main/java/com/example/car_manager/controller/RouteController.java
@@ -0,0 +1,53 @@
+package com.example.car_manager.controller;
+
+
+import com.example.car_manager.model.Car;
+import com.example.car_manager.model.CarData;
+import com.example.car_manager.model.Route;
+import com.example.car_manager.model.User;
+import com.example.car_manager.service.CarService;
+import com.example.car_manager.service.RouteService;
+import com.example.car_manager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/routes")
+public class RouteController {
+
+    private final RouteService routeService;
+    private final UserService userService;
+    private final CarService carService;
+
+    @Autowired
+    public RouteController(RouteService routeService, UserService userService, CarService carService) {
+        this.routeService = routeService;
+        this.userService = userService;
+        this.carService = carService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Route>> getAllRoutes(@RequestParam String username, @RequestParam Integer carId) {
+        // Replace the following stubs with actual service methods to fetch User and Car objects
+        User user = userService.findByUsername(username); // Fetch user by userId
+
+
+        Car car = carService.findById(carId); // Fetch car by carId
+
+
+        List<Route> routes = routeService.getAllRoutes(user, car);
+        return ResponseEntity.ok(routes);
+    }
+
+    @GetMapping("/{routeId}/details")
+    public ResponseEntity<List<CarData>> getRouteDetails(@PathVariable Long routeId) {
+        List<CarData> routeDetails = routeService.getRouteDetails(routeId);
+        return ResponseEntity.ok(routeDetails);
+    }
+
+
+}
+
